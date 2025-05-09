f18e6c40e799555778250482fab998d9a6e82f5d
NeerajQA <neeraj1264qa@gmail.com>
2025-05-09 19:21:34 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   optimizeDeps: {
     esbuildOptions: {
-      platform: 'browser'  // Change from 'linux' to 'browser'
+      platform: 'browser'
     },
   },
-  base: './',
 });
